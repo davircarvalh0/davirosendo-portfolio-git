1ae53d731250283923c9c5c4053e0e5c2aa5e21d
davircarvalh0 <davirosendocarvalho@gmail.com>
2026-04-09 12:18:44 -0300

    refactor(hello-world): troca os nomes das variáveis de português para inglês

--- projeto/hello-world/helloworld.py
+++ projeto/hello-world/helloworld.py
@@ -1,21 +1,21 @@
 print("Qual é o seu nome?")
-usuario = input()
+user = input()
 passed = False
 while passed == False:
-    if len(usuario) > 0:
-        print(f"Hello, {usuario}!")
+    if len(user) > 0:
+        print(f"Hello, {user}!")
         passed = True
     else:
         print("Nome inválido, tente novamente")
-        usuario = input()
+        user = input()
     
 if passed == True:
     print("Quantos anos você tem?")
-    idade = int(input())
-    if idade < 18:
+    age = int(input())
+    if age < 18:
         print("Você não está habilitado a utilizar esta parte do sistema")
     else:
         print()
 
 #Teste
-    print(f"Sua idade é: {idade}")
+    print(f"Sua idade é: {age}")
